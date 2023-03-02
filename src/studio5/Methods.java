43d79670d574d7d8b1134e928801416dcd2500ba
zachhelfand <zachhelfand@10.232.149.159>
2023-03-02 15:29:06 -0600

    Studio 5 done

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -14,8 +14,11 @@ public class Methods {
 	 * @return the Euclidean distance between (x1,y1) and (x2,y2)
 	 */
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
-		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
+		double horizontalDistance = x2 - x1;
+		double verticalDistance = y2 - y1;
+		double distance = Math.sqrt(Math.pow(horizontalDistance, 2) + Math.pow(verticalDistance, 2));
+
 		
 		return distance;
 	}
@@ -34,17 +37,21 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		StdDraw.setPenColor(0, 109, 219);
+		StdDraw.filledCircle(x, y, radius * 3 / 4);
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
+		StdDraw.setPenColor(146, 0, 0);
+		StdDraw.filledCircle(x, y, radius /2);
 
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(255, 255, 109);
+		StdDraw.filledCircle(x, y, radius /4);
 		
 	}
 
@@ -62,6 +69,16 @@ public class Methods {
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
 		// TODO: Finish this method
+		String[] array = new String[source.length()];
+		for (int i = 0; i < source.length(); i++) {
+			array[i] = source.charAt(i) + "";
+			if (array[i].equals(target + "")) {
+				array[i] = replacement;
+			}
+		}
+		for (String val : array) {
+			result += val;
+		}
 		
 		return result;
 	}
@@ -75,6 +92,9 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
+		for (int i = 0; i < values.length; i++) {
+			sum+= values[i];
+		}
 		
 		return sum;
 	}
@@ -87,16 +107,36 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+		int[] values = new int[length]; // FIXME: Create an array of the appropriate size
 		// TODO: Finish this method
+		for (int i = 0; i < length; i++) {
+			values[i] = value;
+		}
 
 		
 
 		return values;
 	}
 
+	
+	/**
+	 * Return a mean blablabla
+	 * 
+	 * @param values the array of ints
+	 * @return mean of ints in values
+	 *
+	 */
 	// TODO: Create an arrayMean method which accepts an int array of values parameter.
 	// TODO: Create a JavaDoc comment for the arrayMean method.
+	public static double arrayMean(int[] values) {
+		double sum = 0;
+		for (int val : values) {
+			sum += val;
+		}
+
+		return sum / values.length;
+	}
+
 
 	
 }
